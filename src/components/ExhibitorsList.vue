f33b4cf2318bc9a5ef1b3bf82634b303adc21221
sergey-exu <3990147@gmail.com>
2021-01-28 14:16:07 +0300

    up design + use active status for modal

--- src/components/ExhibitorsList.vue
+++ src/components/ExhibitorsList.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="exhibitors-list">
+  <div class="exhibitors-list max-w-screen-xl mx-auto">
     
     <ul class="grid grid-cols-1 sm:grid-cols-4 gap-4 px-4 sm:px-0 mb-5">
-      <li v-for="item in exhibitors.data" :key="item.id">
-        <div @click="getExhibitorData(item.id)" class="border shadow p-5 cursor-pointer">
-          <div class="mb-2">
-            <img v-if="item.logo" :src="'https://ekatmaster.ru' + item.logo" class="sm:h-32 h-24 mx-auto" />
-            <svg v-else class="h-24 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <li v-for="item in exhibitors.data" :key="item.id" class="border shadow p-5" v-bind:class="{ 'cursor-pointer': item.active }">
+        <div @click="(item.active) ? getExhibitorData(item.id) : null" >
+          <figure class="mb-2">
+            <img v-if="item.logo" :src="'https://ekatmaster.ru' + item.logo" class="object-contain h-32 w-full" />
+            <svg v-else class="object-contain h-32 text-gray-200 w-full p-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
-          </div>          
+          </figure>          
           <div>{{ item.name }}</div>
           <div class="text-sm text-gray-400">{{ item.city}}</div>
           <div class="text-sm text-gray-400">{{ item.stand }}</div>
