b019a9328c81ebd08363bd6ca078a1ba5899a79b
SrgUsn <3990147@gmail.com>
2021-02-12 21:59:12 +0300

    added element frm

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/tailwind.css'
+import { ElTree } from "element-plus";
+import "element-plus/lib/theme-chalk/el-tree.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component(ElTree.name, ElTree);
+app.mount("#app");
+
